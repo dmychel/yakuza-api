646e88d23b288664ddf3f2a061b1373198095801
dmychel <dmychel@tutanota.com>
2024-04-03 14:34:37 -0400

    component created for code snippet

---
+++ src/components/CodeSnippet.jsx
@@ -0,0 +1,24 @@
+function CodeSnippet() {
+  return (
+    <section className="code_snippet">
+      <div className="code_block">
+        <pre>
+          {`	
+{
+    "item": {	
+        _id	      "660b01fa4aa15727d67398b5"
+        item:	      1
+        category:     "character"
+        name:	      "Kiryu"
+        description   "Kazuma Kiryu (桐生 一馬, Kiryū Kazuma), also known as..."
+        images:	      [..]
+    }
+}
+`}
+        </pre>
+      </div>
+    </section>
+  );
+}
+
+export default CodeSnippet;
