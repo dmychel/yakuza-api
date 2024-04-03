117f1d83645f5953e72233fd43fc02dcbf36439d
dmychel <dmychel@tutanota.com>
2024-04-03 19:27:01 -0400

    basic comp structure that makes use of usestate and useeffect

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,17 +1,37 @@
+import { useEffect } from "react";
 import { callAPI } from "../api/fetch";
+import { useState } from "react";
+
+import "/src/styles/example.css";
 
 function Example() {
-  async function kiryu() {
-    await callAPI("characters/1");
-  }
-  kiryu();
+  const [loading, setLoading] = useState(true);
+  const [object, setObject] = useState(null);
+  useEffect(() => {
+    callAPI("characters/1").then((result) => {
+      setLoading(false);
+      setObject(result);
+    });
+  }, []);
 
   return (
     <section className="example">
-      <div className="character_profile">
-        <p className="character_name"></p>
-        <p className="character_description"></p>
-      </div>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div className="character_profile">
+          <div className="profile_background">
+            <img src="/assets/back.jpg" alt="" />
+          </div>
+          <div className="profile_header">
+            <img src={object.images[0]} alt="Kiryu Image" />
+          </div>
+          <div className="profile_info">
+            <h2 className="character_name">{object.name}</h2>
+            <p className="character_description">{object.description}</p>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
