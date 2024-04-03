512497172cfd5dbead0708c4b3049171bdb2ff03
dmychel <dmychel@tutanota.com>
2024-04-03 19:26:17 -0400

    basic comp structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Content from "./components/Content";
+import "/src/styles/app.css";
+
 function App() {
-  return;
+  return (
+    <>
+      <header>
+        <h1>Yakuza API</h1>
+        <p>
+          By <span>Dylon Crowley-Perez</span>
+        </p>
+      </header>
+      <main>
+        <Content />
+      </main>
+    </>
+  );
 }
 
 export default App;
