94bfdad4ee069fdba32e9ae8dd0001e48fa4772e
dmychel <dmychel@tutanota.com>
2024-04-03 23:23:37 -0400

    jsx update

--- src/components/CodeSnippet.jsx
+++ src/components/CodeSnippet.jsx
@@ -3,16 +3,16 @@ function CodeSnippet() {
     <section className="code_snippet">
       <div className="code_block">
         <pre>
-          {`	
-{
+          {`fetch("https://rest-api-jvtz.onrender.com/characters/1")
+ 
+ {           
   _id:          "660b01fa4aa15727d67398b5"
   category:     "character"
-  description   "Kazuma Kiryu (桐生 一馬, Kiryū Kazuma), also known as..."
+  description   "Kazuma Kiryu (桐生 一馬, Kiryū Kazuma), also known as Joryu (浄龍, Jōryū), and Taichi Suzuki (鈴木 太一, Suzuki Taichi), is the former primary protagonist of the Like a Dragon (formerly Yakuza) series. He is an agent of the Daidoji Faction, the former fourth chairman of the Tojo Clan, the former manager of Morning Glory Orphanage, and a former taxi driver for Nagasu Taxi."
   images:       [..]
   item:	        1
-  name:	        "Kiryu"
-}
-`}
+  name:	        "Kazama Kiryu"
+ }`}
         </pre>
       </div>
     </section>
@@ -20,3 +20,7 @@ function CodeSnippet() {
 }
 
 export default CodeSnippet;
+
+{
+
+}
