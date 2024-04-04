f4d336099e75aa87d4839f2febb132e9b5667932
dmychel <dmychel@tutanota.com>
2024-04-03 23:24:01 -0400

    jsx update

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,10 +5,18 @@ import '/src/styles/demo.css'
 function Demo() {
     return (
         <section className="demo">
-            <h2>Demo</h2>
-            <p>Below is a quick demo of what you can do with this API and the kind of assets we provide. If you need help using this API read the docunenation here.</p>
-            <CodeSnippet />
+            <div className="demo_title">
+                <h2>Demo</h2>
+            </div>
+            <div className="demo_info">
+                <p>
+                    Below is a quick demo of what you can do with this API and the kind of assets we provide. If you need help using this API read the documenation here.</p>
+                <p>
+                    All the assets and character info in this info card are from the API via HTTP fetch request.
+                </p>
+            </div>
             <Example />
+            <CodeSnippet />
         </section>
     )
 }
