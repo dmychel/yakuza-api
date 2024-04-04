5164d9dc33e26af9ecf6e056999942b1e92887eb
dmychel <dmychel@tutanota.com>
2024-04-04 11:02:53 -0400

    added comments

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -19,13 +19,21 @@ function Example() {
       {loading ? (
         "Loading..."
       ) : (
+        // background
         <div className="character_profile drop_shadow">
           <div className="profile_background">
             <img src="/assets/back.jpg" alt="" />
           </div>
+
+          {/* image */}
           <div className="profile_header" style={{ backgroundImage: "url('/assets/features_bg.jpg')" }}>
-            <img src={object.images[0]} alt="Kiryu Image" className="character_img" />
+            <img src={object.images[0]} alt={object.name} />
           </div>
+
+          {/* seperator */}
+          <span className="seperator"></span>
+
+          {/* info */}
           <div className="profile_info">
             <h2 className="character_name">{object.name}</h2>
             <p className="character_description">{object.description}</p>
