c97dfed507a91ce6b2dfe579a33bbf17104434f2
dmychel <dmychel@tutanota.com>
2024-04-03 22:40:24 -0400

    migrated jsx to its own components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,13 @@
-import CodeSnippet from "./CodeSnippet";
-import Example from "./Example";
+import Demo from "./Demo";
+import Intro from "./Intro";
 import "/src/styles/code-block.css";
 import "/src/styles/content.css";
 
 function Content() {
   return (
     <section className="content">
-      <h2>Title</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-        optio debitis, reiciendis fugit quam iusto vel eos, suscipit magnam at
-        tempore. Animi dolor distinctio sit quod tenetur quia iure incidunt.
-      </p>
-      <CodeSnippet />
-      <Example />
+      <Intro />
+      <Demo />
     </section>
   );
 }
