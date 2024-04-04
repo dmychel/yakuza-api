f9c5ecc3ca500f9e109cf7171583c58350748a9c
dmychel <dmychel@tutanota.com>
2024-04-03 22:40:59 -0400

    created component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+function Intro() {
+    return (
+        <section className="intro">
+            <p>
+                A restful API designed to serve media, location, and character info from the Yakuza series.
+            </p>
+        </section>
+    )
+}
+
+export default Intro;
