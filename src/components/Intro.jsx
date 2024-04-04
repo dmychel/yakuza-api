2a2946b84231fda38058232c611968ae238df4cf
dmychel <dmychel@tutanota.com>
2024-04-03 23:24:08 -0400

    jsx update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,7 @@
 function Intro() {
     return (
         <section className="intro">
+            <h1>Yakuza API</h1>
             <p>
                 A restful API designed to serve media, location, and character info from the Yakuza series.
             </p>
