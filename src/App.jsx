78fc254ac7e7bde2ad8d818a958b5b8106016d95
dmychel <dmychel@tutanota.com>
2024-04-06 11:44:36 -0400

    format

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ function App() {
           <Route path='/docs' element={<Docs />}></Route>
           <Route path="*" element={<Error />}></Route>
         </Routes>
-
       </main>
     </BrowserRouter>
   );
