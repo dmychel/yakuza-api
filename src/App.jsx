a2e37679092115cbf4bd46c22b7c20846fee3b9d
dmychel <dmychel@tutanota.com>
2024-04-06 11:44:15 -0400

    set up react router routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import Content from "./components/Content";
+// npm package
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Routes
+import Index from "./routes/Index";
+import Docs from "./routes/Docs";
+import Error from "./routes/Error";
+
+// css
 import "/src/styles/app.css";
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header>
         <p>
           By <span>Dylon Crowley-Perez</span>
         </p>
       </header>
+
       <main>
         <div className="background">
           <img src="/assets/purchase_bg.jpg" alt="" />
         </div>
-        <Content />
+        <Routes>
+          <Route path='/' element={<Index />}></Route>
+          <Route path='/docs' element={<Docs />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
