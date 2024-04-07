f7f9e787f0603abe518517b4e8e3add1303a91f2
dmychel <dmychel@tutanota.com>
2024-04-07 00:23:28 -0400

    added background

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 function Error() {
   return (
     <section className="error">
+      <div className="error_background">
+        <img src="/assets/kiryu-404.jpg" alt="" />
+      </div>
       <h2>Looks like this page doesn&apos;t exist</h2>
       <Link to="/">Go Back</Link>
     </section>
