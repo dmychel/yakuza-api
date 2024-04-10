97e50074b8df9aed637a67a16bc34a637e857646
dmychel <dmychel@tutanota.com>
2024-04-10 16:33:10 -0400

    major jsx change, created serperate comp to make it more readable

--- src/routes/Docs.jsx
+++ src/routes/Docs.jsx
@@ -1,36 +1,32 @@
+import GetALL from "../components/docs/GetAll";
+import GetEntry from "../components/docs/GetEntry";
+import GetRandom from "../components/docs/GetRandom";
+
 function Docs() {
   return (
     <section className="doc_wrapper">
-      <div className="docs">
-        <div className="title">
-          <h2>
-            Yakuza <span style={{ color: "crimson" }}>API</span>
-          </h2>
-        </div>
+      <div className="doc_background">
+        <img src="assets/back.jpg" />
+      </div>
 
-        <div className="doc_child">
-          <h3>Get Character or Location</h3>
+      <div className="doc_info">
+        <h2 style={{ fontFamily: "Yakuza" }}>
+          Yakuza <span style={{ color: "crimson" }}>API</span>
+        </h2>
+        <p>
+          There are two main endpoints you can request data from:{" "}
+          <span style={{ fontFamily: "Yakuza", color: "crimson" }}>
+            character
+          </span>
+          , and{" "}
+          <span style={{ fontFamily: "Yakuza", color: "crimson" }}>
+            location
+          </span>
+        </p>
 
-          <div className="doc_info">
-            <p>
-              There are two categories you can request entries from: Character
-              and Location
-            </p>
-
-            <p>Each category requests an entry via a number.</p>
-          </div>
-
-          <div className="doc_http">
-            <span className="doc_child_title">HTTP Request</span>
-            <div className="code_block doc_code">
-              <span>
-                GET https://rest-api-jvtz.onrender.com/
-                <span style={{ color: "#66FF00" }}>&lt;category&gt;</span>/
-                <span style={{ color: "#66FF00" }}>&lt;entry&gt;</span>
-              </span>
-            </div>
-          </div>
-        </div>
+        <GetEntry />
+        <GetRandom />
+        <GetALL />
       </div>
     </section>
   );
