834f9628b293b39fe444e3d0516753e748c2d097
dmychel <dmychel@tutanota.com>
2024-04-10 14:45:42 -0400

    jsx update

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import "/src/styles/app.css";
 
 function Error() {
   return (
-    <section
-      className="error_wrapper"
-      style={{ backgroundImage: `url("assets/kiryu-404.jpg")` }}
-    >
+    <section className="error_wrapper">
+      <div className="error_background">
+        <img src="assets/kiryu-404.jpg" />
+      </div>
       <div className="error">
         <h2>Looks like this page doesn&apos;t exist</h2>
         <Link to="/"> Go Back</Link>
