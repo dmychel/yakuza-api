7b57ba04d7dfbb22d43cc2b6af8cf8bb47a6fa63
dmychel <dmychel@tutanota.com>
2024-04-10 17:14:31 -0400

    created jsx

--- src/routes/Roadmap.jsx
+++ src/routes/Roadmap.jsx
@@ -1,5 +1,15 @@
 function Roadmap() {
-  return "Road map";
+  return (
+    <div className="roadmap_wrapper">
+      <div className="roadmap_background">
+        <img src="assets/images/kiryu-404.jpg" alt="" />
+      </div>
+      <div className="roadmap">
+        <h2>We&apos;re Working on it..</h2>
+        <h3>Check back soon</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Roadmap;
