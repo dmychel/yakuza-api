303c085a4ac7c5c5c1569114abc112fecf6995dc
Dylon <dmychel@tutanota.com>
2024-11-17 17:47:30 -0500

    import styles

--- src/routes/Docs.jsx
+++ src/routes/Docs.jsx
@@ -2,7 +2,7 @@ import GetALL from "../components/docs/GetAll";
 import GetEntry from "../components/docs/GetEntry";
 import GetRandom from "../components/docs/GetRandom";
 
-import styles from "/src/styles/index.module.scss";
+import styles from "/src/styles/docs.module.scss";
 
 export default function Docs() {
   return (
@@ -10,25 +10,26 @@ export default function Docs() {
       <div class={styles.doc_background}>
         <img src="assets/images/back.jpg" />
       </div>
+      <div class={styles.doc}>
+        <div class={styles.doc_info} style={{ fontSize: "20px" }}>
+          <h2 style={{ fontFamily: "Yakuza" }}>
+            Yakuza <span style={{ color: "crimson" }}>API</span>
+          </h2>
+          <p>
+            There are two main endpoints you can request data from:{" "}
+            <span style={{ fontFamily: "Yakuza", color: "crimson" }}>
+              character
+            </span>
+            , and{" "}
+            <span style={{ fontFamily: "Yakuza", color: "crimson" }}>
+              location
+            </span>
+          </p>
 
-      <div class={styles.doc_info} style={{ fontSize: "20px" }}>
-        <h2 style={{ fontFamily: "Yakuza" }}>
-          Yakuza <span style={{ color: "crimson" }}>API</span>
-        </h2>
-        <p>
-          There are two main endpoints you can request data from:{" "}
-          <span style={{ fontFamily: "Yakuza", color: "crimson" }}>
-            character
-          </span>
-          , and{" "}
-          <span style={{ fontFamily: "Yakuza", color: "crimson" }}>
-            location
-          </span>
-        </p>
-
-        <GetEntry />
-        <GetRandom />
-        <GetALL />
+          <GetEntry />
+          <GetRandom />
+          <GetALL />
+        </div>
       </div>
     </div>
   );
