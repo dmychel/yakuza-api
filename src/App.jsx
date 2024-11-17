2f28c104c3cb5ec64e9e94357f9bd3c53294d657
Dylon <dmychel@tutanota.com>
2024-11-17 02:55:44 -0500

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // routes
 import Index from "/src/routes/Index";
 import MyRevelations from "/src/routes/MyRevelations";
+import Docs from "./routes/Docs";
 import About from "/src/routes/About";
+import Demo from "/src/routes/Demo";
 
 // components
 import Header from "/src/components/Header";
@@ -21,7 +23,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Index />}></Route>
             <Route path="/my-revelations" element={<MyRevelations />}></Route>
+            <Route path="/docs" element={<Docs />}></Route>
             <Route path="/about" element={<About />}></Route>
+            <Route path="/demo" element={<Demo />}></Route>
             <Route path="*" element={<Error />}></Route>
           </Routes>
         </main>
