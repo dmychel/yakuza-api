3f75246a6dcb686240642627563eabe0012bc7c4
Dylon <dmychel@tutanota.com>
2024-11-16 23:46:44 -0500

    created component

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import styles from "/src/styles/header.module.scss";
+import background from "/assets/images/cast-bg.png";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header style={{ backgroundImage: `url(${background})` }}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/my-revelations">My Revelations</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
