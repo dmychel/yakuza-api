845732fc6ed40fb548568e88d946b6604db4785f
Dylon <dmychel@tutanota.com>
2024-11-17 00:19:50 -0500

    changed jsx and imported sass

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,8 +1,23 @@
-// components
-import Home from "/src/components/Home";
+import styles from "/src/styles/index.module.scss";
+import Demo from "/src/components/Demo";
 
-function Index() {
-  return <Home />;
+export default function Index() {
+  return (
+    <div class={styles.home_wrapper}>
+      <div class={styles.home_background}>
+        <img src="assets/images/kiryu-disco.jpg" />
+      </div>
+
+      <div class={styles.intro}>
+        <h1 style={{ fontFamily: "Yakuza" }}>
+          Yakuza <span style={{ color: "crimson" }}>API</span>
+        </h1>
+        <p>
+          A restful API designed to serve media, location, and character info
+          from the Yakuza series.
+        </p>
+      </div>
+      <Demo />
+    </div>
+  );
 }
-
-export default Index;
