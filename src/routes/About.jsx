c935ac78d5d2a08e2fea5fe0ba1f0c80dd9145cf
dmychel <dmychel@tutanota.com>
2024-11-16 20:23:50 -0500

    .

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -13,19 +13,20 @@ function About() {
           <p>
             I created this API for two reasons: Firstly, there were no Yakuza
             APIs at the time of conception, and secondly, I aimed to deepen my
-            understanding of the backend development and it&apos;s ecosystem.
-            When delving into a new technology or concept, the development
-            community often emphasizes the importance of learning by creating
-            projects that address real-world problems. However, brainstorming
-            project ideas for technologies you have very little to no experience can be challenging. What issues are there to solve when
-            you&apos;re you&apos;ve never used the technology?
+            understanding of backend development and its ecosystem. When delving
+            into a new technology or concept, the development community often
+            emphasizes the importance of learning by creating projects that
+            address real-world problems. However, brainstorming project ideas
+            for technologies you have very little to no experience with can be
+            challenging. What issues are there to solve when you&apos;ve never
+            used the technology?
           </p>
           <p>
             After several attempts with mundane CRUD apps, I recalled my search
             for a Yakuza API to utilize its resources, which led to the
             inception of this project. Although still in its beta stage,
             I&apos;m uncertain about the extent to which I intend to expand it.
-            Nevertheless, I plan to add and maintain this project.
+            Nevertheless, I plan to add to and maintain this project.
           </p>
         </div>
       </div>
